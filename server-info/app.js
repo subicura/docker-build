a1bff4795709537aacd3f93a066d59a8b658b553
subicura <subicura@subicura.com>
2020-09-16 21:21:12 +0900

    init

---
+++ server-info/app.js
@@ -0,0 +1,20 @@
+const os = require("os");
+
+// Require the framework and instantiate it
+const fastify = require("fastify")({
+  logger: true,
+});
+
+// Declare a route
+fastify.get("/", function (request, reply) {
+  reply.send({ hostname: os.hostname() });
+});
+
+// Run the server!
+fastify.listen(3000, "0.0.0.0", function (err, address) {
+  if (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+  fastify.log.info(`server listening on ${address}`);
+});
